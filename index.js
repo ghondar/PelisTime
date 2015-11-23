f76166bd155982c16cd9fc621d23cfeaa9d3f510
Anthony Brian Gavilan Vinces <abgv9221@gmail.com>
2015-11-22 21:38:52 -0500

    init electron version

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   if (process.env.DEBUG) {
-    mainWindow.loadUrl('file://' + __dirname + '/dev-index.html');
+    mainWindow.loadURL('file://' + __dirname + '/dev-index.html');
   } else {
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
   }
 
   if (process.env.NODE_ENV === 'development') {
